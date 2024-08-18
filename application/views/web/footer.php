e4cf5085e9c9921594641852f58480ec4fb44445
Mutmainnah <mut.2707@gmail.com>
2024-08-18 13:32:10 +0700

    First commit

---
+++ application/views/web/footer.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<div class="footer text-muted text-center">
+	<a href="https://ua.ac.id//" title="Universitas Annuqayah" target="_blank">Universitas Annuqayah</a>
+</div>
+<!-- /footer -->
+</div>
+<!-- /content area -->
+</div>
+<!-- /main content -->
+</div>
+<!-- /page content -->
+</div>
+<!-- /page container -->
+</body>
+
+</html>
